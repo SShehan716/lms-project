f0a54c499ea469e77b006fbd6cb5e9d35a9f2ae1
SShehan <sachinshehan20@gmail.com>
2024-06-28 00:55:36 +0530

    frontend and backend initialize

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,29 @@
+// frontend/src/pages/Home.js
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+
+const Home = () => {
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        api.get('/')
+            .then(response => setMessage(response.data))
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setError(error.message);
+            });
+    }, []);
+
+    return (
+        <div>
+            {error ? (
+                <div>Error: {error}</div>
+            ) : (
+                <div>{message}</div>
+            )}
+        </div>
+    );
+};
+
+export default Home;
