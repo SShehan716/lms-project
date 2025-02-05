d36e76ddb18363fb3de8267bae2b30b9df817f0b
SShehan716 <sachinshehan20@gmail.com>
2025-02-05 19:24:19 +0530

    get all users api added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 const { getUserByEmail } = require('../services/userService');
+const { getAllUsers } = require('../services/userService');
 
 // get user by Email
 exports.getUserByEmail = async (req, res) => {
@@ -9,6 +10,17 @@ exports.getUserByEmail = async (req, res) => {
         }else{
             res.status(404).json({message: 'User Not Found'});
         }
+    }catch(error){
+        res.status(500).json({message: 'Server Error'});
+    }
+}
+
+//get all users
+exports.getAllUsers = async (req, res) => {
+    try{
+        console.log('get all users');
+        const users = await getAllUsers();
+        res.status(200).json(users);
     }catch(error){
         res.status(500).json({message: 'Server Error'});
     }
