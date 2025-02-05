abe4fc1d22856bbec4c2cbe0d9f014f7d6297ff1
SShehan716 <sachinshehan20@gmail.com>
2025-02-05 22:42:41 +0530

    all user list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import LoginPage from './pages/LoginPage';
-import Profile from './pages/Profile';
-import Dashboard from './pages/DashboardPage';
+import LoginPage from './pages/authPages/LoginPage';
+import InvitePage from './pages/InvitePage';
+import Profile from './pages/profile/ProfilePage';
+import VerifyUserPage from './pages/authPages/VerifyUserPage';
+import UserListPage from './pages/adminPages/UsersListPage';
+
 import Header from './components/common/AppHeader';
 import Footer from './components/common/Footer';
 
@@ -15,7 +18,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/profile" element={<Profile />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/invite-user" element={<InvitePage />} />
+                <Route path="/verify/:token" element={<VerifyUserPage />} />
+                <Route path="/user-list" element={<UserListPage />} />
           </Routes>
           <Footer />
         </Router>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,16 +1,22 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-
-    const login = (useData) => {
-        setUser(useData);
-    }
+    const [user, setUser] = useState(() => {
+        const storedUser = localStorage.getItem('user');
+        return storedUser ? JSON.parse(storedUser) : null;
+      });
+    
+      const login = (userData) => {
+        setUser(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+      };
+    
 
     const logout = () => {
         setUser(null);
+        localStorage.removeItem('user');
     }
 
     return (
