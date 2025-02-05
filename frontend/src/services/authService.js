72b5f67920da08f7ff66a9ba702bfe477fa3cf53
SShehan716 <sachinshehan20@gmail.com>
2025-02-05 19:23:48 +0530

    frontend auth service updated

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -9,9 +9,18 @@ export const login = async (email, password) => {
     }
 }
 
-export const register = async (name, email, password) => {
+export const register = async ({ name, email, password, role }) => {
     try {
-        const response = await api.post('api/users/register', { name, email, password });
+        const response = await api.post('api/auth/register', { name, email, password, role });
+        return response.data;
+    } catch (error) {
+        return error.response.data;
+    }
+}
+
+export const verifyUserToken = async (token) => {
+    try {
+        const response = await api.get(`api/auth/verify/${token}`);
         return response.data;
     } catch (error) {
         return error.response.data;
