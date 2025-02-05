df3a103384c7fc5b441be4cd4fcffc03af907734
SShehan716 <sachinshehan20@gmail.com>
2025-02-05 22:52:30 +0530

    get user by email add to frontend

--- frontend/src/components/profile/ProfileInfo.js
+++ frontend/src/components/profile/ProfileInfo.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Card, Descriptions, Typography } from 'antd';
-
-const { Title } = Typography;
+import { Card, Descriptions } from 'antd';
 
 const ProfileInfo = ({ user }) => {
     return (
